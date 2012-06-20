e3b5cb59d58df957b9c37e2a14f652f0f333d9bd
Rasmus Sten <rasmus@dll.nu>
2012-06-19 21:18:47 -0700

    change connectionPending to long because that's the type returned by the underlying data structure

--- Palvelukartta.m
+++ Palvelukartta.m
@@ -165,7 +165,7 @@ NSString* ctostr(NSURLConnection* c) {
     [data appendData:newdata];
 }
 
-- (int) connectionsPending {
+- (long) connectionsPending {
     return [remainingConnections count];
 }
 
